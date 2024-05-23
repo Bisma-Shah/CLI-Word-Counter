2141d1e561f7c44171b15ed5a4ae6ec19b51894d
bisma Shah <165302776+Bisma-Shah@users.noreply.github.com>
2024-05-23 21:38:34 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+// Importing inquirer and chalk packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Display a Colourfull Welcome message
+console.log(chalk.bold.rgb(284, 284,284)(` \n  \t\t <<<==========================>>>`));
+console.log(chalk.bold.rgb(284, 284,284)(`<<<=======>>> ${chalk.bold.hex(`#9999FF`)(`Welcome to \`Bisma Shah\` - Words-Counter App`)}  <<<=========>>>`));
+console.log(chalk.bold.rgb(284, 284,284)(`\t\t <<<==============================>>>\n`));
+
+// Prompt the user to enter a Sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.yellow("Enter a Sentence:"),
+
+    }
+]);
+
+// Trimming the sentence and splitting it into words based on "spaces"
+let words = answers.sentence.trim().split(" ");
+
+// Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold.white("- Sentence words:"));
+console.log(words);
+console.log(chalk.bold(`\n - word Count: ${chalk.bold.yellowBright(words.length)}`));
+console.log("=".repeat(60));
